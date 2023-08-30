7e43edb4dbf7e8fdf9d3b5db11e509fc4423ab71
Hanna Kasabutskaya <akosobutskaya@yandex-team.ru>
2023-08-30 22:07:02 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
-
 import "./pages/index.css";
 import { renderCards } from "./components/card.js";
 import { FormValidator } from "./components/FormValidator.js";
 import { PopupWithImage } from "./components/PopupWithImage.js";
 import { PopupWithForm } from "./components/PopupWithForm.js";
-import { Api } from "./components/Api_.js";
-import { addPopupEvents } from "./components/modal.js";
-import { getProfilInfo, getCards } from "./components/api.js";
+import { Api } from "./components/Api.js";
 import { setUserInfo } from "./components/profile.js";
-<<<<<<< HEAD
-import { validationConfig, popupImageSelector, popupEditProfileSelector, apiData, editBtn, userName, userJob } from "./components/constants.js"
-=======
-import { cardsGrid, validationConfig } from "./components/constants.js"
-import Section from "./components/card.js"
-import Card from "./components/card.js"
+import {
+  validationConfig, popupImageSelector, popupEditProfileSelector,
+  apiData, editBtn, userName, userJob
+} from "./components/constants.js"
+import { Section } from "./components/section.js"
+import { Card } from "./components/card.js"
 
->>>>>>> 7124a7abd12135d0240e2edb240f9a97c6cfee1d
+/*------------------------------- Убрать после перехода на Api----------*/
+const config = {
+    baseUrl: 'https://nomoreparties.co/v1/plus-cohort-27',
+    headers: {
+        authorization: '945d8c08-a2b0-4b31-8bc8-ff311437b5f8',
+        'Content-Type': 'application/json'
+    }
+}
 
+const getData = (res) => {
+  if (res.ok) return res.json();
+  return Promise.reject(`Ошибка: ${res.status}`);
+}
+
+function getProfilInfo() {
+  return fetch(`${config.baseUrl}/users/me`, { headers: config.headers }).then(getData);
+}
+
+function getCards() {
+  return fetch(`${config.baseUrl}/cards`, { headers: config.headers }).then(getData);
+}
+
+/*------------------------------------------------------------*/
 Promise.all([getProfilInfo(), getCards()])
   .then(([profileData, cards]) => {
     setUserInfo(profileData);
@@ -25,17 +43,6 @@ Promise.all([getProfilInfo(), getCards()])
   .catch(err => {
     console.log(err);
   });
-
-//addPopupEvents();
-
-const api = new Api(apiData);
-
-// Validation
-const formList = Array.from(document.querySelectorAll(validationConfig.formSelector));
-formList.forEach((formElement) => {
-  const formValidator = new FormValidator(validationConfig, formElement);
-  formValidator.enableValidation();
-});
 
 //addPopupEvents();
 
@@ -73,7 +80,7 @@ const popupEditProfile = new PopupWithForm(
 );
 
 const setProfileFormData = () => {
-  const userData = {name: "11", about: "222"}//getUserInfo();
+  const userData = { name: "11", about: "222" }//getUserInfo();
   userName.value = userData.name;
   userJob.value = userData.about;
 };
